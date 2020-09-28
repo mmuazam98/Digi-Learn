30d0eb6def011f16e4ca98d363499ccbecbb1941
Mohammad MuaZam <mm9084@srmist.edu.in>
2020-09-28 20:28:11 +0530

    final commit

--- public/js/index.js
+++ public/js/index.js
@@ -159,12 +159,44 @@ $("#days span").click(function(){
 
 
 
+function createCookie(name, value, days) {
+    var expires;
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
 
-$(window).on('load',function(){
-    // $('#myModal').modal('show');
+function getCookie(c_name) {
+    if (document.cookie.length > 0) {
+        c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1;
+            c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = document.cookie.length;
+            }
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return "";
+}
+if(getCookie()==" ")
+{
     setTimeout(function(){
         $('#myModal').modal('show');
-   }, 0);
+    }, 0);
+}
+$(window).on('load',function(){
+    // $('#myModal').modal('show');
+//     setTimeout(function(){
+//         $('#myModal').modal('show');
+//    }, 0);
    $('#myModal').modal({
         backdrop: 'static',
         keyboard: false
